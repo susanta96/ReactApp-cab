8d367f22784ef22bfb9bb8b01271619785eda6c5
susanta96 <susant.vanu7278@gmail.com>
2019-01-15 16:21:25 +0530

    client and server package.json updated & server.js

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
+//immport
 const express = require('express');
+const bodyParser = require("body-parser");
+const mysql = require('mysql');
+// const http = require('http');
+// var path = require('path');
+const routes = require("./routes/routes");
 
+//setting up express
 const app = express();
 
-app.get('/api/customers', (req, res ) => {
-    const customers = [
-        {id: 1, firstName: 'Susanta', lastName: 'Chakraborty'},
-        {id: 2, firstName: 'Ayan', lastName: 'Das'},
-        {id: 3, firstName: 'Vishal', lastName: 'Sarkar'},
-    ];
-
-    res.json(customers);
+//setting up db connection
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'reactapp-cab'
 });
+ 
+connection.connect();
+ 
+global.db = connection;
 
 
 
-const PORT = 5000;
+// Middlewares for Static files
+// app.use("/client/assets", express.static("assets"));
+
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//call the routes
+app.use(routes);
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
